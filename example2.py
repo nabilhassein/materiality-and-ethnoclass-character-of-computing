b61ef33c4892e517b4979368113d5919ef1c8b2a
Nabil Hassein <nabilhassein@users.noreply.github.com>
2019-08-19 09:48:45 -0400

    Create example2.py

---
+++ example2.py
@@ -0,0 +1,13 @@
+### PART 2: numbers, math, datatype conversion
+answer = 7
+guess = int(input("guess the number from 1-10 "))
+
+while guess != answer:
+    guess = int(input("try again!"))
+
+print("you got it!!!")
+
+## exercises:
+# Modify the range of the game, and play a few rounds with your neighbors.
+# Change the game from "guess the number" to "guess the word".
+# challenge: give the user some feedback about their guess.
