563104c042e468f6a593028134525bfdd25a06e1
Nabil Hassein <nabilhassein@users.noreply.github.com>
2019-08-19 09:46:03 -0400

    Create example0.py

---
+++ example0.py
@@ -0,0 +1,10 @@
+### PART 0: up and running. function calls, variables, strings, output.
+print("hello, world")  # the traditional first program
+
+person = "nabil"  # this is an "assignment" statement, with one =
+                  # the variable named "person" stores the string value "nabil"
+print("hello, " + person)  # this is called string concatenation
+
+## exercises:
+# Modify the above code to greet a neighbor at your seat.
+# Say something kind to them after their name. (There are at least two ways to do this.)
